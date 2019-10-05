7d59f7fdba6d886135a56fcf9456aabd65855b2b
Pawel-Mk <pawelmk78@gmail.com>
2019-10-05 11:29:15 +0200

    Updated to for loop

--- English_Interactive_Dictionary.py
+++ English_Interactive_Dictionary.py
@@ -8,31 +8,26 @@ import json
 import difflib
 
 os.getcwd()
-os.chdir(r'D:\Github_Projects\10 Apps\Interactive_English_Dictionary'.replace(os.sep , os.altsep))
+os.chdir(r'D:\Github_Projects\10 Apps/'.replace(os.sep , os.altsep))
 data = json.load(open('data.json'))
-
 
 def keydefinition(key):
     if key.lower() in data.keys():
         return data[key]
-    else:
-        if difflib.get_close_matches(key.lower(), data.keys(), cutoff=0.8) != []:
-            similar = difflib.get_close_matches(key.lower(), data.keys(), cutoff=0.8)[0].capitalize()
-            comfirmation = input(f"This dictionary doesn't contain word '{key}'. Did you mean '{similar}'? Please write Y if yes, or N if no.\n[Y]/[N]: ")
-            while comfirmation not in ['Y', 'N']:
-                comfirmation = input('Please enter capitalized Y or N: ')
-            if comfirmation == 'Y':
-                return data[similar.lower()]
-            else:
-                return 'Thank you for using my dictionary.'
+    elif difflib.get_close_matches(key.lower(), data.keys(), cutoff=0.8) != []:
+        similar = difflib.get_close_matches(key.lower(), data.keys(), cutoff=0.8)[0].capitalize()
+        comfirmation = input(f"This dictionary doesn't contain word '{key}'. Did you mean '{similar}'? Please write Y if yes, or N if no.\n[Y]/[N]: ")
+        if comfirmation == 'Y':
+            return data[similar.lower()]
+        elif comfirmation == 'N':
+            return 'Thank you for using my dictionary.'
         else:
-            return 'This dictionary does not contain such word or similar to it.'
-
-
+            return 'Your entrance was not recognized as Y or N.'
+    else:
+        return 'This dictionary does not contain such word or similar to it.'
 
 key = input('Enter a word you would like to translate: ')
 
-keydefinition(key)
 
 if isinstance(keydefinition(key), list):
     print(key.capitalize() + ':')
@@ -40,5 +35,7 @@ if isinstance(keydefinition(key), list):
         print('\n' + i)
 else:
     print(keydefinition(key))
-    
-#this is branching test
+
+
+
+
